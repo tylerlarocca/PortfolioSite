f64969c2c164e8d25e7661c613d91723f57f0313
Tyler LaRocca <tylerlarocca@Tylers-MacBook-Pro.local>
2025-01-25 13:48:13 -0800

    added tech stack

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
-import React from "react"; 
-import "./Home.css";  
-import TylerPfp from "../../Assets/TylerPfp.jpeg"
+import React from "react";
+import "./Home.css";
+import TylerPfp from "../../Assets/TylerPfp.jpeg";
 import HeaderSocials from "./HeaderSocials";
 import ScrollDown from "./ScrollDown";
 
@@ -10,31 +10,40 @@ function Home() {
       <div className="intro">
         <img src={TylerPfp} alt="" className="home__img" />
         <h1 className="home__name">Tyler LaRocca</h1>
-        <span className="home__education">Full Stack JavaScript Developer and AWS Certified Cloud Practitioner</span>
+        <span className="home__education">
+          Full Stack JavaScript Developer and AWS Certified Cloud Practitioner
+        </span>
 
         <HeaderSocials />
 
         <div class="tech-stack">
-    <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-    <i class="devicon-javascript-plain colored"></i>
-    <i class="devicon-css3-plain colored"></i>       
-    <i class="devicon-html5-plain colored"></i>
-    <i class="devicon-amazonwebservices-plain-wordmark colored"></i>  
-    <i class="devicon-bootstrap-plain colored"></i>    
-    <i class="devicon-react-original colored"></i>        
-    <i class="devicon-postgresql-plain-wordmark colored"></i>        
-    <i class="devicon-knexjs-plain-wordmark colored"></i>       
-    <i class="devicon-express-original-wordmark colored"></i>      
-</div>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+          />
+          <i class="devicon-javascript-plain colored"></i>
+          <i class="devicon-typescript-plain colored"></i>
+          <i class="devicon-react-original colored"></i>
+          <i class="devicon-python-plain colored"></i>
+          <i class="devicon-nextjs-original-wordmark colored"></i>
+          <i class="devicon-css3-plain colored"></i>
+          <i class="devicon-html5-plain colored"></i>
+          <i class="devicon-amazonwebservices-plain-wordmark colored"></i>
+          <i class="devicon-bootstrap-plain colored"></i>
+          <i class="devicon-postgresql-plain-wordmark colored"></i>
+          <i class="devicon-knexjs-plain-wordmark colored"></i>
+          <i class="devicon-express-original-wordmark colored"></i>
+        </div>
 
+        <a href="#contact" className="btn">
+          Contact
+        </a>
 
-        <a href="#contact" className="btn">Contact</a>
- 
-        
         <ScrollDown />
       </div>
     </section>
-  )
+  );
 }
 
 export default Home;
