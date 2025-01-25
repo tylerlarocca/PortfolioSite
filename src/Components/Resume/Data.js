91c533ecd022e9005edd36a88b01dd807c57bfba
Tyler LaRocca <tylerlarocca@Tylers-MacBook-Pro.local>
2025-01-25 13:36:34 -0800

    resume and schooling updates

--- src/Components/Resume/Data.js
+++ src/Components/Resume/Data.js
@@ -1,60 +1,56 @@
 const Data = [
-    {
-        id: 1, 
-        category: "education", 
-        icon: "icon-graduation",
-        year: "2023 - 2024",
-        title: "Thinkful Software Engineering Bootcamp",
-        desc: "Acquired comprehensive knowledge of industry best practices and software development standards, focusing on JavaScript, HTML5, CSS3, React Native, Node.js, PostgreSQL, RESTful APIs, algorithms, and data structures."
-    },
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2025-2027",
+    title: "Arizona State University",
+    desc: "Master's of Computer Science degree in progress",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2023 - 2024",
+    title: "Thinkful Software Engineering Bootcamp",
+    desc: "Acquired comprehensive knowledge of industry best practices and software development standards, focusing on JavaScript, HTML5, CSS3, React Native, Node.js, PostgreSQL, RESTful APIs, algorithms, and data structures.",
+  },
 
-    {
-        id: 2, 
-        category: "education", 
-        icon: "icon-graduation",
-        year: "2021-2022",
-        title: "University of California, Riverside",
-        desc: "M.Ed, Master's Degree in Higher Education Administration and Policy completed with a 3.5GPA"
-    },
+  {
+    id: 3,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2018-2022",
+    title: "University of California, Riverside",
+    desc: "M.Ed and B.A, Master's Degree in Higher Education Administration and Policy & Bachelor's in Learning and Behavioral Studies",
+  },
 
-    {
-        id: 3, 
-        category: "education", 
-        icon: "icon-graduation",
-        year: "2018-2021",
-        title: "University of California, Riverside",
-        desc: "Bachelor's Degree in Education with a Concentration in Learning and Behavioral Studies completed with a 3.5GPA"
-    },
+  {
+    id: 4,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2024 - Current",
+    title: "Software Engineer - FrontEnd | ComplyAI",
+    desc: "Develop and enhance web applications using TypeScript, JavaScript, React, and TailwindCSS. Focus on creating intuitive, user-friendly interfaces that improve the overall user experience.Build efficient, accessible, and responsive web applications optimized for performance.Implement modern front-end development practices, ensuring clean, maintainable code.Collaborate closely with cross-functional teams to deliver high-quality solutions that address real-world problems.Continuously seek ways to improve the user experience and technical quality of web applications.",
+  },
 
-    {
-        id: 4, 
-        category: "experience", 
-        icon: "icon-briefcase",
-        year: "2024 - Current",
-        title: "Software Engineer - FrontEnd | ComplyAI",
-        desc: "Develop and enhance web applications using TypeScript, JavaScript, React, and TailwindCSS. Focus on creating intuitive, user-friendly interfaces that improve the overall user experience.Build efficient, accessible, and responsive web applications optimized for performance.Implement modern front-end development practices, ensuring clean, maintainable code.Collaborate closely with cross-functional teams to deliver high-quality solutions that address real-world problems.Continuously seek ways to improve the user experience and technical quality of web applications."
-    },
+  {
+    id: 5,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2024 - Current",
+    title: "Fullstack Software Engineering Intern (AI) | The Madden Academy",
+    desc: "Collaborate in a dynamic team to develop an AI chatbot using PHP/Laravel and JavaScript. Implementing and integrating Natural Language Models (NLM) to enhance the chatbot's ability to coach users through Madden games. Participating in designing, coding, testing, and deploying chatbot features to ensure a seamless user experience.",
+  },
 
-    {
-        id: 5, 
-        category: "experience", 
-        icon: "icon-briefcase",
-        year: "2024 - Current",
-        title: "Fullstack Software Engineering Intern (AI) | The Madden Academy",
-        desc: "Collaborate in a dynamic team to develop an AI chatbot using PHP/Laravel and JavaScript. Implementing and integrating Natural Language Models (NLM) to enhance the chatbot's ability to coach users through Madden games. Participating in designing, coding, testing, and deploying chatbot features to ensure a seamless user experience."
-    },
+  {
+    id: 6,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "2021 - 2022",
+    title: "Web Developer | Digital Cartel Media",
+    desc: "Designed and implemented innovative web development strategies, optimizing user experience and enhancing website performance. Developed and executed data-driven web applications, increasing user engagement and satisfaction. Implemented front-end and back-end solutions, actively improving website functionality and reliability. Conducted in-depth code reviews and testing to ensure high-quality, bug-free releases, enabling data-driven decision-making.",
+  },
+];
 
-
-    {
-        id: 6, 
-        category: "experience", 
-        icon: "icon-briefcase",
-        year: "2021 - 2022",
-        title: "Web Developer | Digital Cartel Media",
-        desc: "Designed and implemented innovative web development strategies, optimizing user experience and enhancing website performance. Developed and executed data-driven web applications, increasing user engagement and satisfaction. Implemented front-end and back-end solutions, actively improving website functionality and reliability. Conducted in-depth code reviews and testing to ensure high-quality, bug-free releases, enabling data-driven decision-making." 
-    },
-
-
-]
-
-export default Data; 
+export default Data;
